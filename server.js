c0ad7d9180bda2151c06156f530e1f88afe7f221
yjie28 <xieyy28@gmail.com>
2020-03-05 15:49:05 -0800

    Testing

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send(db.users);
+  res.send('This is Working .. !');
 });
 
 app.post('/signin', signin.handleSignin(db, bcrypt));
