75baa5d043e972de8e660eae6873bbdbc7947155
yjie28 <xieyy28@gmail.com>
2020-03-05 16:38:44 -0800

    Modified API key visibility

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: 'ea85131f521d4b2b809199ed1814e131'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = () => (req, res) => {
