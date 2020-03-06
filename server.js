e050c859cff6059cabec2d10dab71c72b77a7ae1
yjie28 <xieyy28@gmail.com>
2020-03-05 16:09:42 -0800

    Updated db

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-transparent-90062',
-    user: '',
-    password: '',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
